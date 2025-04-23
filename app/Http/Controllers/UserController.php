93f4a83402070e48977c43f5f1a72291b5bfefcd
Aditya <adityapermono8@gmail.com>
2025-04-23 19:42:14 +0700

    create, read, search data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
-        return view('user.index');
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+
+        if ($search) {
+            $users = User::where(function($query) use ($search) {
+                $query->where('name', 'like', "%{$search}%")
+                      ->orWhere('email', 'like', "%{$search}%");
+            })
+            ->where('id', '!=', 1)
+            ->orderBy('name')
+            ->paginate(20)
+            ->withQueryString();
+        } else {
+            $users = User::where('id', '!=', 1)
+                         ->orderBy('name')
+                         ->paginate(20);
+        }
+
+        return view('user.index', compact('users'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
 
+Route::resource('todo', TodoController::class)->except(['show']);
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,7 +23,6 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
 Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
